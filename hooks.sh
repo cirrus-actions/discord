28bcde2839bdb2333d50ff42140de6873288a714
Reece Dunham <me@rdil.rocks>
2019-01-29 21:59:04 -0500

    Add some variables

--- hooks.sh
+++ hooks.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Setup Git stuff:
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
+
+# Build passed:
+EMBED_COLOR=3066993
+STATUS_MESSAGE="Passed Without Error"
+AVATAR="https://avatars1.githubusercontent.com/u/29414678?v=4" # logo
