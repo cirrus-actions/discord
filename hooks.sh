c32e13aa656fc160371b83d37e36d13bbca42eb0
Reece Dunham <me@rdil.rocks>
2019-01-30 09:26:29 -0500

    Add null check and functionality

--- hooks.sh
+++ hooks.sh
@@ -16,6 +16,11 @@ CREDITS="This was newly committed."
 # Cirrus CI Logo:
 LOGO_URL"https://avatars1.githubusercontent.com/u/29414678?v=4"
 
+# Check if variable is null
+if [ -z "$WEBHOOK_URL" ]; then
+  echo -e "WARNING!!\nYou need to put WEBHOOK_URL as am environment variable!" && exit
+fi
+
 # Test to see if this is a pull request:
 if [[ $CIRRUS_BRANCH != 'master' ]]; then
   URL="https://github.com/$CIRRUS_REPO_FULL_NAME/pull/$CIRRUS_PR"
@@ -52,3 +57,6 @@ WEBHOOK_DATA='{
     "timestamp": ""
   } ]
 }'
+
+(curl --fail --progress-bar -A "Cirrus-CI-Webhook" -H Content-Type:application/json -H X-Author:jumbocakeyumyum#0001 -d "$WEBHOOK_DATA" "$WEBHOOK_URL" \
+  && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook (failed)."
