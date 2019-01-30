7b391ca2b82c09c1c17f22b565d4b033479486cd
Reece Dunham <jumbocakefdx@gmail.com>
2019-01-30 09:23:25 -0500

    Add major data parts (#2)

--- hooks.sh
+++ hooks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copyright (c) 2019-present CirrusLabs
 
 # Setup Git stuff:
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
@@ -7,5 +8,47 @@ git config --global user.name "GitHub Action"
 
 # Build passed:
 EMBED_COLOR=3066993
-STATUS_MESSAGE="Passed Without Error"
-AVATAR="https://avatars1.githubusercontent.com/u/29414678?v=4" # logo
+STATUS_MESSAGE="Build Passed"
+
+# Credits (not author, just leftover from port):
+CREDITS="This was newly committed."
+
+# Cirrus CI Logo:
+LOGO_URL"https://avatars1.githubusercontent.com/u/29414678?v=4"
+
+# Test to see if this is a pull request:
+if [[ $CIRRUS_BRANCH != 'master' ]]; then
+  URL="https://github.com/$CIRRUS_REPO_FULL_NAME/pull/$CIRRUS_PR"
+else
+  URL="https://github.com/$CIRRUS_REPO_FULL_NAME"
+fi
+
+# Webhook data:
+WEBHOOK_DATA='{
+  "username": "Cirrus CI",
+  "avatar_url": '$LOGO_URL',
+  "embeds": [ {
+    "color": '$EMBED_COLOR',
+    "author": {
+      "name": "Job #'"$CIRRUS_BUILD_ID"' (Task ID: #'"$CIRRUS_TASK_ID"') - '"$CIRRUS_REPO_FULL_NAME"'",
+      "url": "'"$CIRRUS_WORKING_DIR"'",
+      "icon_url": "'$LOGO_URL'"
+    },
+    "title": "'"$CIRRUS_CHANGE_MESSAGE"'",
+    "url": "'"$URL"'",
+    "description": "'"${CIRRUS_CHANGE_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
+    "fields": [
+      {
+        "name": "Commit",
+        "value": "", # todo: add value
+        "inline": true
+      },
+      {
+        "name": "Branch",
+        "value": "'"[\`$CIRRUS_BRANCH\`](https://github.com/$CIRRUS_REPO_FULL_NAME/tree/$CIRRUS_BRANCH)"'",
+        "inline": true
+      }
+    ],
+    "timestamp": ""
+  } ]
+}'
